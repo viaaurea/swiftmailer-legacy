3248d682744a4f175e80e431c60f7e0e32705658
Andrej Rypo <xrypak@gmail.com>
2023-06-06 10:57:08 +0200

    null to zero

--- lib/classes/Swift/SwiftException.php
+++ lib/classes/Swift/SwiftException.php
@@ -23,6 +23,6 @@ class Swift_SwiftException extends Exception
      */
     public function __construct($message, $code = 0, Exception $previous = null)
     {
-        parent::__construct($message, $code, $previous);
+        parent::__construct($message, $code ?? 0, $previous);
     }
 }
